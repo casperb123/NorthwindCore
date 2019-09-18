3ec8828e4fd48959a5d1a4b45c99308dfc20b05f
Casper Bastholm <42714453+casperb123@users.noreply.github.com>
2019-09-18 13:51:10 +0200

    Added documentatio for the order entity

--- NorthwindCore.Entities/Order.cs
+++ NorthwindCore.Entities/Order.cs
@@ -15,21 +15,79 @@ namespace NorthwindCore.Entities
             orderDetails = new ObservableCollection<OrderDetail>();
         }
 
+        /// <summary>
+        /// Gets and sets the order id
+        /// </summary>
         public int OrderId { get; set; }
+
+        /// <summary>
+        /// Gets and sets the customer id
+        /// </summary>
         public string CustomerId { get; set; }
+
+        /// <summary>
+        /// Gets and sets the employee id
+        /// </summary>
         public int? EmployeeId { get; set; }
+
+        /// <summary>
+        /// Gets and sets the order date
+        /// </summary>
         public DateTime? OrderDate { get; set; }
+
+        /// <summary>
+        /// Gets and sets the required date
+        /// </summary>
         public DateTime? RequiredDate { get; set; }
+
+        /// <summary>
+        /// Gets and sets the shipped date
+        /// </summary>
         public DateTime? ShippedDate { get; set; }
+
+        /// <summary>
+        /// Gets and sets the ship via
+        /// </summary>
         public int? ShipVia { get; set; }
+
+        /// <summary>
+        /// Gets and sets the freight
+        /// </summary>
         public decimal? Freight { get; set; }
+
+        /// <summary>
+        /// Gets and sets the ship name
+        /// </summary>
         public string ShipName { get; set; }
+
+        /// <summary>
+        /// Gets and sets the ship address
+        /// </summary>
         public string ShipAddress { get; set; }
+
+        /// <summary>
+        /// Gets and sets the ship city
+        /// </summary>
         public string ShipCity { get; set; }
+
+        /// <summary>
+        /// Gets and sets the ship region
+        /// </summary>
         public string ShipRegion { get; set; }
+
+        /// <summary>
+        /// Gets and sets the ship postal code
+        /// </summary>
         public string ShipPostalCode { get; set; }
+
+        /// <summary>
+        /// Gets and sets the ship country
+        /// </summary>
         public string ShipCountry { get; set; }
 
+        /// <summary>
+        /// Returns the total number of order details
+        /// </summary>
         public int TotalOrderDetails
         {
             get
@@ -38,6 +96,9 @@ namespace NorthwindCore.Entities
             }
         }
 
+        /// <summary>
+        /// Returns the total price for all order details
+        /// </summary>
         public decimal TotalPrice
         {
             get
@@ -48,9 +109,24 @@ namespace NorthwindCore.Entities
             }
         }
 
+        /// <summary>
+        /// Gets and sets the customer navigation property
+        /// </summary>
         public virtual Customer Customer { get; set; }
+
+        /// <summary>
+        /// Gets and sets the employee navigation property
+        /// </summary>
         public virtual Employee Employee { get; set; }
+
+        /// <summary>
+        /// Gets and sets the ship via navigation property
+        /// </summary>
         public virtual Shipper ShipViaNavigation { get; set; }
+
+        /// <summary>
+        /// Gets and sets the order details list
+        /// </summary>
         public virtual ICollection<OrderDetail> OrderDetails
         {
             get
